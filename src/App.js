0a32ea1cb00486a3ada7435a36846ee9b944ea53
Nacho Caiafa <nachokai@outlook.com>
2020-04-09 17:37:25 -0300

    add files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import data from './assets/data.json';
+import JobBoard from './components/Jobboard/JobBoard';
+
+function App() {
+	const [jobs, setJobs] = useState([]);
+
+	useEffect(() => {
+		setJobs(data);
+	}, []);
+
+	return (
+		<div className="App">
+    <header className='bg-teal-500 mb-12'>
+    <img src="/images/bg-header-desktop.svg" />
+    </header>
+			{jobs.length === 0 ? (
+				<p>Searching...</p>
+			) : (
+				jobs.map((job) => <JobBoard job={job} key={job.id} />)
+			)}
+		</div>
+	);
+}
+
+export default App;
