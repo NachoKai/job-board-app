3e9ab63012f54927a2edda0af13a0d306f0675c9
Nacho Caiafa <nachokai@outlook.com>
2020-04-09 17:39:17 -0300

    Updates

---
+++ precache-manifest.2ad9bf67fb1310704e82836797717d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86d13cedddbb441c0c2f66f30c1a400",
+    "url": "/job-board-app/index.html"
+  },
+  {
+    "revision": "0cbdd4d57e58b23fa8c2",
+    "url": "/job-board-app/static/css/main.955fcca3.chunk.css"
+  },
+  {
+    "revision": "e9ba0cbfe66b438326eb",
+    "url": "/job-board-app/static/js/2.31011cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-board-app/static/js/2.31011cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbdd4d57e58b23fa8c2",
+    "url": "/job-board-app/static/js/main.8f469efa.chunk.js"
+  },
+  {
+    "revision": "f8628294f3e4e2ac9998",
+    "url": "/job-board-app/static/js/runtime-main.7bc1aedb.js"
+  }
+]);
